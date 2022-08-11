0c10f1a8afc4b91af4f05e93707e11041bcfd04b
treyer <nemereno_by@mail.ru>
2022-08-11 21:09:11 +0300

    feat: add expressionType and calculatorType to store

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,14 +1,22 @@
 import { createAction, handleActions } from 'redux-actions'
 
 const INITIAL_STATE = {
-  appType: 'functional',
-  theme: 'light',
+  appType: 'functional', // 'functional' or 'class'
+  theme: 'light', // 'light' or 'dark'
+  expressionType: 'simple', // 'simple' or 'complex'
+  calculatorType: 'basic', // 'basic' or 'advanced'
 }
 
 export const changeComponentsType = createAction(
   'CHANGE_COMPONENTS_TYPE',
 )
 export const changeTheme = createAction('CHANGE_THEME')
+export const changeExpressionType = createAction(
+  'CHANGE_EXPRESSION_TYPE',
+)
+export const changeCalculatorType = createAction(
+  'CHANGE_CALCULATOR_TYPE',
+)
 
 export default handleActions(
   {
@@ -25,6 +33,24 @@ export default handleActions(
       return {
         ...state,
         theme: state.theme === 'light' ? 'dark' : 'light',
+      }
+    },
+    [changeExpressionType]: state => {
+      return {
+        ...state,
+        expressionType:
+          state.expressionType === 'simple'
+            ? 'complex'
+            : 'simple',
+      }
+    },
+    [changeCalculatorType]: state => {
+      return {
+        ...state,
+        calculatorType:
+          state.calculatorType === 'basic'
+            ? 'advanced'
+            : 'basic',
       }
     },
   },
