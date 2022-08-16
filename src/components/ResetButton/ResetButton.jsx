a0089c1bc81d886311fd5968b100c2a7447d0d91
treyer <nemereno_by@mail.ru>
2022-08-16 17:28:00 +0300

    feat: implement clear all history functionality

--- src/components/ResetButton/ResetButton.jsx
+++ src/components/ResetButton/ResetButton.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+
 import Flex from '@/components/Flex/Flex'
 import { Button } from './components'
 
+import {
+  clearHistory,
+  updateUserInput,
+} from '@/store/actions/data'
+
 export const ResetButton = () => {
+  const dispatch = useDispatch()
+
+  const handleClearHistory = () => {
+    dispatch(clearHistory())
+    dispatch(updateUserInput([{ number: '0' }]))
+  }
+
   return (
-    <Button>
+    <Button onClick={handleClearHistory}>
       <Flex> Clear All History</Flex>
     </Button>
   )
