c11460b5790a605f16a5cb7c49490bca1432d285
treyer <nemereno_by@mail.ru>
2022-10-12 02:41:01 +0300

    feat: add save in localStorage and use current theme tests

--- cypress/integration/ThemeSelect/theme-class.spec.js
+++ cypress/integration/ThemeSelect/theme-class.spec.js
@@ -98,3 +98,131 @@ describe('ThemeSelect class components', () => {
     )
   })
 })
+
+describe('ThemeSelect with localStorage', () => {
+  beforeEach(() => {
+    cy.visit('/settings')
+    cy.contains('Class components implementation').click()
+    cy.clearLocalStorage().should(
+      localStorage =>
+        expect(localStorage.getItem('theme')).to.be.null,
+    )
+  })
+
+  it('check localStorage theme variable set to correct value', () => {
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(67, 67, 67)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 255, 255)',
+    )
+
+    cy.getBySel('theme-select-class')
+      .should('be.visible')
+      .contains('Light theme')
+    cy.getBySel('theme-select-class').click()
+    cy.getBySel('option-select-dark-class')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('theme')).to.eq('dark')
+      })
+
+    cy.getBySel('theme-select-class')
+      .should('be.visible')
+      .contains('Dark theme')
+    cy.getBySel('option-select-light-class').should(
+      'not.be.visible',
+    )
+    cy.getBySel('option-select-dark-class').should(
+      'not.exist',
+    )
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(24, 24, 24)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(128, 128, 128)',
+    )
+
+    cy.getBySel('theme-select-class').click()
+    cy.getBySel('option-select-light-class')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('theme')).to.eq('light')
+      })
+    cy.getBySel('theme-select-class')
+      .should('be.visible')
+      .contains('Light theme')
+    cy.getBySel('option-select-light-class').should(
+      'not.exist',
+    )
+    cy.getBySel('option-select-dark-class').should(
+      'not.be.visible',
+    )
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(67, 67, 67)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 255, 255)',
+    )
+  })
+
+  it('check correct localStorage theme after app reload', () => {
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(67, 67, 67)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 255, 255)',
+    )
+    cy.getBySel('theme-select-class')
+      .should('be.visible')
+      .contains('Light theme')
+    cy.getBySel('theme-select-class').click()
+    cy.getBySel('option-select-dark-class')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('theme')).to.eq('dark')
+      })
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(24, 24, 24)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(128, 128, 128)',
+    )
+
+    cy.visit('/settings')
+    cy.contains('Class components implementation').click()
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(24, 24, 24)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(128, 128, 128)',
+    )
+    cy.should(() =>
+      expect(localStorage.getItem('theme')).to.eq('dark'),
+    )
+  })
+})
--- cypress/integration/ThemeSelect/theme.spec.js
+++ cypress/integration/ThemeSelect/theme.spec.js
@@ -87,3 +87,125 @@ describe('ThemeSelect', () => {
     )
   })
 })
+
+describe('ThemeSelect with localStorage', () => {
+  beforeEach(() => {
+    cy.visit('/settings')
+    cy.clearLocalStorage().should(
+      localStorage =>
+        expect(localStorage.getItem('theme')).to.be.null,
+    )
+  })
+
+  it('check localStorage theme variable set to correct value', () => {
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(67, 67, 67)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 255, 255)',
+    )
+
+    cy.getBySel('theme-select')
+      .should('be.visible')
+      .contains('Light theme')
+    cy.getBySel('theme-select').click()
+    cy.getBySel('option-select-dark')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('theme')).to.eq('dark')
+      })
+
+    cy.getBySel('theme-select')
+      .should('be.visible')
+      .contains('Dark theme')
+    cy.getBySel('option-select-light').should(
+      'not.be.visible',
+    )
+    cy.getBySel('option-select-dark').should('not.exist')
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(24, 24, 24)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(128, 128, 128)',
+    )
+
+    cy.getBySel('theme-select').click()
+    cy.getBySel('option-select-light')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('theme')).to.eq('light')
+      })
+    cy.getBySel('theme-select')
+      .should('be.visible')
+      .contains('Light theme')
+    cy.getBySel('option-select-light').should('not.exist')
+    cy.getBySel('option-select-dark').should(
+      'not.be.visible',
+    )
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(67, 67, 67)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 255, 255)',
+    )
+  })
+
+  it('check correct localStorage theme after app reload', () => {
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(67, 67, 67)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 255, 255)',
+    )
+    cy.getBySel('theme-select')
+      .should('be.visible')
+      .contains('Light theme')
+    cy.getBySel('theme-select').click()
+    cy.getBySel('option-select-dark')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('theme')).to.eq('dark')
+      })
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(24, 24, 24)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(128, 128, 128)',
+    )
+
+    cy.visit('/settings')
+    cy.get('header').should(
+      'have.css',
+      'background-color',
+      'rgb(24, 24, 24)',
+    )
+    cy.get('main').should(
+      'have.css',
+      'background-color',
+      'rgb(128, 128, 128)',
+    )
+    cy.should(() =>
+      expect(localStorage.getItem('theme')).to.eq('dark'),
+    )
+  })
+})
